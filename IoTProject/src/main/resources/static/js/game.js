4555c8cf252ad01bb88ac54235b7c1f2ad685ea5
robinhood128 <robbedecraemer@hotmail.com>
2018-04-20 11:47:23 +0200

    peeeeee

--- IoTProject/src/main/resources/static/js/game.js
+++ IoTProject/src/main/resources/static/js/game.js
@@ -1,4 +1,18 @@
 var stompClient = null;
+var stickman = null
+var ctx = null;
+var peescore = 0;
+
+window.onload = function() {
+    var c = document.getElementById("game");
+    ctx = c.getContext("2d");
+
+    stickman = new Image();
+    stickman.onload = function() {
+        drawCanvas();
+    };
+    stickman.src = '/img/stickman_pee.png';
+};
 
 function connect() {
     disconnect();
@@ -10,8 +24,31 @@ function connect() {
             console.log(player);
             $("#name").text(player.name);
             $("#score").text(player.score);
+            peescore = player.score;
+            drawCanvas();
         });
     });
+}
+
+function drawCanvas() {
+    if (ctx != null && stickman != null && stickman.complete) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.drawImage(stickman, 10, 10);
+
+        var start = {x:85, y:150};
+        var point1 = {x:100, y:150};
+        var point2 = {x:100 + peescore * 2, y:150};
+        var end = {x:200 + peescore * 2, y:300};
+
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.bezierCurveTo(point1.x, point1.y, point2.x, point2.y, end.x, end.y);
+        ctx.lineWidth = 5;
+
+        // line color
+        ctx.strokeStyle = 'yellow';
+        ctx.stroke();
+    }
 }
 
 function disconnect() {
@@ -21,11 +58,19 @@ function disconnect() {
     }
 }
 
+/*function onScoreChange(score) {
+    console.log(score);
+    peescore = score;
+    drawCanvas();
+}*/
 
 $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
 
-    $( "#receive" ).click(function() { connect(); });
+    $("#receive").click(function() { connect(); });
+
+    /*var slider = document.getElementById('peescore');
+    slider.onchange = function () { onScoreChange(this.value) };*/
 });
